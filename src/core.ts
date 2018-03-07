dbe78810534abebdbe0eb01e556d233528c94469
James Batt <getjamesbatt@gmail.com>
2018-03-07 21:31:06 +1100

    bug fixes

--- src/core.ts
+++ src/core.ts
@@ -7,12 +7,19 @@ export interface ObservableChangeHandler {
   (change: MobxEvent): void;
 }
 
-const registry = new Map<MetadataKey, ObservableChangeHandler>();
+interface MetadataItem {
+  property: string;
+  changeHandler(event: MobxEvent): void;
+}
+
+type Metadata = MetadataItem[];
 
 export function decoratorFactory(metadataKey: MetadataKey, changeHandler: ObservableChangeHandler) {
-  registry.set(metadataKey, changeHandler);
   return (target: any, property: string) => {
-    Reflect.defineMetadata(metadataKey, { property }, target);
+    const metadataItem: MetadataItem = { property, changeHandler };
+    const existingMetadata = Reflect.getMetadata(metadataKey, target) || [];
+    const metadata: Metadata = [...existingMetadata, metadataItem];
+    Reflect.defineMetadata(metadataKey, metadata, target);
   };
 }
 
@@ -22,15 +29,13 @@ spy((mobxEvent: MobxEvent) => {
     return;
   }
   const metadataKeys = Reflect.getMetadataKeys(object);
-  for (const key of metadataKeys) {
-    const metadata = Reflect.getMetadata(key, object);
-    const registeredFunction = registry.get(key);
-    if (registeredFunction && name === metadata.property) {
-      // only run the registered function if the
-      // property name of the observable matches
-      // the property name the function was registered
-      // for (i.e. it's the property with the mobx-state-decorator).
-      registeredFunction(mobxEvent);
+  metadataKeys.forEach((key) => {
+    const metadata = Reflect.getMetadata(key, object) as Metadata | undefined;
+    if (!metadata) {
+      return;
     }
-  }
+    metadata
+      .filter((item) => item.property === name)
+      .forEach((item) => item.changeHandler(mobxEvent));
+  });
 });
