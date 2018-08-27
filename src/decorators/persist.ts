401851fe942f1d1e36ea03e7fbcbf78ea0441596
James Batt <getjamesbatt@gmail.com>
2018-08-27 14:54:40 +1000

    added "defaultValue" to persist config to work around the lack of property initializer support for decorators

--- src/decorators/persist.ts
+++ src/decorators/persist.ts
@@ -4,29 +4,20 @@ import * as serializer from '../serializer';
 
 export interface PersistConfig {
   key: string;
+  defaultValue?: any;
 }
 
 export function persist(config: PersistConfig) {
   return decoratorFactory('mobx-decorators:persist', (mobxEvent: MobxEvent) => {
     switch (mobxEvent.type) {
       case 'add':
-        const retrievedValue = localStorage.getItem(config.key) || '{}';
-        const { value } = serializer.fromJson(retrievedValue);
-        // we do it in the next run of the event loop because the
-        // otherwise initial value for class properties will override
-        // the initial valaue we want.
-        // i.e.
-        // class MyStore {
-        //   @persist(...)
-        //   @observable
-        //   myProperty: string = '';
-        // }
-        // The initial/default value for myProperty is an empty string ''
-        // and this will be set on the observable after our spy pulls the
-        // localstorage value. So we want to make sure our local storage
-        // value happens after this.
-        // Yes it's a massive hack :()
-        setTimeout(() => mobxEvent.object[mobxEvent.key] = value, 0);
+        if (!(config.key in localStorage)) {
+          mobxEvent.object[mobxEvent.key] = config.defaultValue;
+        } else {
+          const retrievedValue = localStorage.getItem(config.key) || '{}';
+          const { value } = serializer.fromJson(retrievedValue);
+          mobxEvent.object[mobxEvent.key] = value;
+        }
         break;
 
       case 'update':
